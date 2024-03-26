1d72c37607555fd721bbb56126020af78a5818f3
marie@MBY-VICTUS <Benyoussef.Mariem@esprit.tn>
2024-03-26 06:07:49 +0100

    ajouter crud publication

---
+++ src/Form/PublicationType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Publication;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PublicationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('contenu')
+            ->add('nbLikes')
+            ->add('nbDislike')
+            ->add('datePub')
+            ->add('photo')
+            ->add('idClient')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Publication::class,
+        ]);
+    }
+}
